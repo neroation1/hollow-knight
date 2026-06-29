8c0e154713a1a33dfeb83fe5c56e5154cec37990
neroation1 <neroation@gmail.com>
2026-06-29 23:47:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Eurdwino!');
+  res.send('Hello Eurdino!');
 });
 
 app.get("/items", async (req, res) => {
